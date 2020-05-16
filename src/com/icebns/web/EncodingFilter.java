fada70eec6233f8ce03828671ef5843252ba2185
icebns <735652650@qq.com>
2020-05-16 16:15:59 +0800

    自动登录

--- src/com/icebns/web/EncodingFilter.java
+++ src/com/icebns/web/EncodingFilter.java
@@ -20,32 +20,32 @@ public class EncodingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)throws IOException,ServletException{
         servletRequest.setCharacterEncoding("utf-8");
-        UserService service = new UserServiceImpl();
-        HttpServletRequest req = (HttpServletRequest)servletRequest;
-        HttpSession session = req.getSession();
-        User user = (User)session.getAttribute("sessionuser");
-        Cookie[] cookies = req.getCookies();
-        Map<String,Cookie> cookieMap = CookieUtils.toCookie(cookies);
-//        Cookie cookie = cookieMap.get("username");
-        if (user == null){
-            Cookie cookie = cookieMap.get("username");
-            if (cookie==null){
-                req.getRequestDispatcher("index.jsp").forward(req,servletResponse);
-                filterChain.doFilter(servletRequest,servletResponse);
-            }else{
-                String username = cookie.getValue();
-                User selectuser = service.getUserByUsername(username);
-                if (selectuser==null){
-                    req.getRequestDispatcher("index.jsp").forward(req,servletResponse);
-                    filterChain.doFilter(servletRequest,servletResponse);
-                }else{
-                    session.setAttribute("sessionuser",selectuser);
-                    filterChain.doFilter(servletRequest,servletResponse);
-                }
-            }
-        }else{
+//        UserService service = new UserServiceImpl();
+//        HttpServletRequest req = (HttpServletRequest)servletRequest;
+//        HttpSession session = req.getSession();
+//        User user = (User)session.getAttribute("sessionuser");
+//        Cookie[] cookies = req.getCookies();
+//        Map<String,Cookie> cookieMap = CookieUtils.toCookie(cookies);
+////        Cookie cookie = cookieMap.get("username");
+//        if (user == null){
+//            Cookie cookie = cookieMap.get("username");
+//            if (cookie==null){
+//                req.getRequestDispatcher("index.jsp").forward(req,servletResponse);
+//                filterChain.doFilter(servletRequest,servletResponse);
+//            }else{
+//                String username = cookie.getValue();
+//                User selectuser = service.getUserByUsername(username);
+//                if (selectuser==null){
+//                    req.getRequestDispatcher("index.jsp").forward(req,servletResponse);
+//                    filterChain.doFilter(servletRequest,servletResponse);
+//                }else{
+//                    session.setAttribute("sessionuser",selectuser);
+//                    filterChain.doFilter(servletRequest,servletResponse);
+//                }
+//            }
+//        }else{
             filterChain.doFilter(servletRequest,servletResponse);
-        }
+//        }
 
 
 
